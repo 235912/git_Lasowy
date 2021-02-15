05286d1c5e813d4a09832b95ced1ef49955c0c8a
infor <235912@edu.p.lodz.pl>
2021-02-15 18:47:55 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/git_Lasowy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_Lasowy.dir/main.c.obj"
+  "git_Lasowy.exe"
+  "git_Lasowy.exe.manifest"
+  "git_Lasowy.pdb"
+  "libgit_Lasowy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/git_Lasowy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/git_Lasowy.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for git_Lasowy.
+# This may be replaced when dependencies are built.
